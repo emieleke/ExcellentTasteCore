6a50b9aab628d210880e9d1019db222d3cda2a7f
[dylanhagmolen] <0318584@student.rocvantwente.nl>
2022-01-19 12:28:26 +0100

    Creates BarmanOverzicht

---
+++ ExcellentTasteCore/Models/BarmanOverzicht.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcellentTasteCore.Models
+{
+    public class BarmanOverzicht
+    {
+        public int BestellingId { get; set; }
+        public int ReserveringId { get; set; }
+        public string ConsumptieItemCode { get; set; }
+        public int Aantal { get; set; }
+        public DateTime? DateTimeBereidingConsumptie { get; set; }
+
+        public virtual ConsumptieItem ConsumptieItem { get; set; }
+        public virtual Reservering Reservering { get; set; }
+    }
+}
